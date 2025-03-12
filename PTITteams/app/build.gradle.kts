f807cff65c59d141b1b53da5dd4d4bfa8500fa49
QuocAnhtapcode <quocanhtapcode@gmail.com>
2025-03-12 16:03:34 +0700

    Onboarding screen

---
+++ PTITteams/app/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "club.mobile.d21.ptitteams"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "club.mobile.d21.ptitteams"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    viewBinding{
+        enable = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    // Navigation Component
+    implementation (libs.androidx.navigation.fragment.ktx)
+    implementation (libs.androidx.navigation.ui.ktx)
+
+    // ViewModel & LiveData
+    implementation (libs.androidx.lifecycle.viewmodel.ktx)
+    implementation (libs.androidx.lifecycle.livedata.ktx)
+    //ViewPager2
+    implementation (libs.androidx.viewpager2)
+}
---
+++ PTITteams/app/src/main/java/club/mobile/d21/ptitteams/model/OnBoardingItem.kt
@@ -0,0 +1,8 @@
+package club.mobile.d21.ptitteams.model
+
+data class OnboardingItem(
+    val image1ResId: Int,
+    val image2ResId: Int,
+    val title: String,
+    val description: String
+)
